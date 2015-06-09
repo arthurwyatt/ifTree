ddef510db3a661a0aab80bc7145bb297a24f0b50
Arthur Wyatt <arthur_wyatt@hotmail.com>
2015-06-09 14:16:53 -0700

    Additional examples, and a tweak to the view source code

--- examples/js/utils.js
+++ examples/js/utils.js
@@ -1,5 +1,11 @@
 //show source
 function showSource (tree, elem) {
-	 var source = JSON.stringify(tree.options, null, 4).split("<").join("&lt").split(">").join("&gt");
-	 document.getElementById(elem).innerHTML = source;
+	//clone options
+	var options =JSON.parse(JSON.stringify(tree.options));
+	//get rid of nowhere
+	delete options.nodes.nowhere;
+	console.log(options.nodes.nowhere);
+
+	var source = JSON.stringify(options, null, 4).split("<").join("&lt").split(">").join("&gt");
+	document.getElementById(elem).innerHTML = source;
 }
